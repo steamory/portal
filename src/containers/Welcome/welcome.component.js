309555c02b442eb951ae1c7f9aec76f69bb11c2f
ivy <xieyang@dodora.cn>
2020-01-31 17:26:00 +0800

    feature: added redirect pipline

--- src/containers/Welcome/welcome.component.js
+++ src/containers/Welcome/welcome.component.js
@@ -26,11 +26,11 @@ export const WelcomePageContent = props => {
     <WelcomeWrapper data-testid="welcome-wrapper">
       <WelcomeCard className="card">
         <WelcomeLogo data-testid="welcome-logo">
-          <img src="/img/logo.svg" alt="Inrupt" />
+          <img src="https://usercontents.authing.cn/logo/authing@256x78.png" alt="Inrupt" />
         </WelcomeLogo>
         <WelcomeProfile data-testid="welcome-profile">
           <h3>
-            {t('welcome.welcome')}, <WelcomeName>{name}</WelcomeName>
+            {t('welcome.welcome')}, <WelcomeName>{name.split('.')[0].split('://')[1]}</WelcomeName>
           </h3>
           <ImageWrapper>
             <Uploader
@@ -73,111 +73,6 @@ export const WelcomePageContent = props => {
             />
           </ImageWrapper>
         </WelcomeProfile>
-      </WelcomeCard>
-      <WelcomeCard className="card">
-        <WelcomeDetail data-testid="welcome-detail">
-          <Trans i18nKey="welcome.title">
-            <h3>
-              title
-              <a
-                href="https://github.com/inrupt/solid-react-sdk"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                link
-              </a>
-            </h3>
-          </Trans>
-          <Trans i18nKey="welcome.description">
-            <p>
-              text
-              <a
-                href="https://github.com/inrupt/solid-react-sdk"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                link{' '}
-              </a>{' '}
-              text
-            </p>
-          </Trans>
-          <Trans i18nKey="welcome.libraryList">
-            <ul>
-              <li>
-                <a
-                  href="https://github.com/inrupt/solid-react-components"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Reusable Components
-                </a>
-                that you can use on your own in the applications that you build.
-              </li>
-              <li>
-                <a
-                  href="https://github.com/inrupt/generator-solid-react"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Application Generator
-                </a>
-                that incorporates all of the components and best practices together for you,
-                standing up THIS fully functional Solid React application. Note: The Solid React
-                application illustrates the use of the components installed by the Generator. It
-                should not be considered as a service provided by inrupt, and is subject to change.
-              </li>
-              <li>
-                Best practice patterns that you can reference as examples of how to accomplish
-                particular things.
-              </li>
-            </ul>
-          </Trans>
-
-          <Trans i18nKey="welcome.evolvingMessage">
-            <p>
-              The SDK is continually evolving. Take a look at the
-              <a
-                href="https://github.com/inrupt/solid-react-sdk/tree/master#release-timeline"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Release Timeline
-              </a>
-              for what’s been implemented as part of the previous releases, and what's currently
-              planned.
-            </p>
-          </Trans>
-          <p>{t('welcome.version050')}</p>
-          <h3>{t('welcome.fairUsageTitle')}</h3>
-          <p>{t('welcome.fairUsageText')}</p>
-          <Trans i18nKey="welcome.fairUsageExamples">
-            <ul>
-              <li>
-                If your application is Solid compatible, you can use the Solid logo to indicate
-                that. If not, please don’t misrepresent yourself by using the Solid logo.
-              </li>
-              <li>
-                Please don't use the inrupt name as a part of your company name, website name,
-                domain name, service name, or app name. Don't use our logo as your own or modify it
-                to fit into your own logo.
-              </li>
-              <li>
-                If you’d like, you may use the inrupt logo to indicate that your application is
-                powered by inrupt’s SDK, but please make sure the context is clear to the user. Your
-                own logo should always be larger than inrupt’s logo, for example.
-              </li>
-            </ul>
-          </Trans>
-          <p>{t('welcome.fairUsageSummary')}</p>
-          <h3>{t('welcome.contactUsTitle')}</h3>
-          <Trans i18nKey="welcome.contactUsText">
-            <p>
-              If you have additional questions about the use of the React SDK for Solid, the
-              Application Generator, or inrupt’s brand, please contact
-              <a href="mailto:support@inrupt.com">support@inrupt.com</a>.
-            </p>
-          </Trans>
-        </WelcomeDetail>
       </WelcomeCard>
     </WelcomeWrapper>
   );
